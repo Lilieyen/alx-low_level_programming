ea24d10f1f86564af30e292e83c0a17dba1adbd5
Lilieyen <lnyasita@gmail.com>
2020-09-16 20:55:33 +0000

    print line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
 *main - starting point
-*Description: outputs "\"programming is like building a mutilingual puzzle"
+*Description: outputs "\"programming is like building a multilingual puzzle"
 *Return: 0
 */
 int main (void)
