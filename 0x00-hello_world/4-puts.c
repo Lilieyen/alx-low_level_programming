9983fe01635a23725bf96452a05984b85e12dede
Lilieyen <lnyasita@gmail.com>
2020-09-16 19:48:11 +0000

    script printing text line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - starting point
  * Description: outputs "\"programming is like building a mutilingual puzzle"
  * Return: 0
  */
-
 int main (void)
-
 {
   puts("\"programming is like building a multilingual puzzle");
   return (0);
