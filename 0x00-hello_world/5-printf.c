e20164cca9519562d5ba46373269b013e36179dd
Lilieyen <lnyasita@gmail.com>
2020-09-16 20:26:13 +0000

    print a line

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
 *main- program starting point
 *Return: 0
@@ -8,4 +7,4 @@ int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
-} 
+}
