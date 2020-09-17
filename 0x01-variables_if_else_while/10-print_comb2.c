03838409a5640b5a335f04423ecb628bd3320aad
Lilieyen <lnyasita@gmail.com>
2020-09-17 20:23:23 +0000

    program that prints the numbers from 00 to 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *Description: comb 00-99
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		putchar(x);
+		if (i == 57 && x == 57)
+		{
+			break;
+		}
+		putchar(44);
+		putchar(32);
+		x++;
+	}
+	i++;
+}
+putchar('\n');
+return (0);
+}
