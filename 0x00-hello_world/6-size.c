0457c1bdcafc2393634782fd37ae18b7ba8ed0ed
Lilieyen <lnyasita@gmail.com>
2020-09-17 07:20:34 +0000

    print sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
-/**
-*main- starting point
-*Description: outputs the sizes of different datatypes
-*Return: 0
-*/
-int main(void)
-{
-printf("size of a char: %d byte(s)\n", sizeof(char));
-printf("size of an int: %d byte(s)\n", sizeof(int));
-printf("size of a long int: %d  byte(s)\n", sizeof(long));
-printf("size of a long long int: %d byte(s)\n", sizeof(long long));
-printf("size of a float: %d byte(s)\n", sizeof(float));
-return (0);
+#include<stdio.h>
+int main() {
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
+
+//sizeof evaluates the size of a variable
+	printf ("size of int: %1d bytes\n", sizeof(intType));
+	printf ("size of float: %1d bytes\n", sizeof(floatType));
+	printf ("size of double: %1d bytes\n", sizeof(doubleType));
+	printf ("size of char: %1d bytes\n", sizeof(charType));
+	return 0;
 }
