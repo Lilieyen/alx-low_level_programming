af793ebe8d4cb9c0558e9b732544c5709f4f450a
Lilieyen <lnyasita@gmail.com>
2020-09-18 17:27:01 +0000

    assign random number to variable n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int n;
 
-	srand(time (0));
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%i is possitive\n", n);
@@ -18,5 +18,5 @@ int main(void)
 		printf("%i is zero\n", n);
 	else
 		printf("%i is negative\n", n);
-		       return (0);
-		       }
+	return (0);
+}
