f1ec01d9a9e39eeb66b6c1bae2c3f52c03f9055f
Lilieyen <lnyasita@gmail.com>
2020-09-18 19:23:41 +0000

    printing using function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,9 @@
  *Description: outputs "\"programming is like building a multilingual puzzle"
  *Return: 0
  */
+
 int main(void)
+
 {
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
