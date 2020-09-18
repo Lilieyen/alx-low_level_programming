702ed256de99464d0a4820a1fc0155379e443d46
Lilieyen <lnyasita@gmail.com>
2020-09-18 20:53:44 +0000

    print using function puts alone

---
+++ 0x01-variables_if_else_while/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - starting poiny
+ *Description: outputs "\"programming is like building a multilingual puzzle"
+ *Return:0
+ */
+
+int main(void)
+
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
