9e75ce10f1848c96e36fe0b034a138bacac5825a
Lilieyen <lnyasita@gmail.com>
2020-09-23 18:39:31 +0000

    function that prints 10 times the alphabet, in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- *print_alphabet - a functin to print the alphabet 10 times
- *Description: argument
+ *print_alphabet_x10 - a functin to print the alphabet 10 times
+ *
  *return: void
  */
 
