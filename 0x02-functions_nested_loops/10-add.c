d577d5739a7ba7b8cd7234e798d6e7d89843489b
Lilieyen <lnyasita@gmail.com>
2020-09-23 12:40:54 +0000

    function that adds two intergers and returns results

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ *add - add two intergers and return results
+ *@a: parameter 1
+ *@b: parameter 2
+ *Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
