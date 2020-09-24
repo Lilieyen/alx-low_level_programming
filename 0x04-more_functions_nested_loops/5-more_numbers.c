dc160beeafe06cf5573e3f260cbaf319f300cccf
Lilieyen <lnyasita@gmail.com>
2020-09-24 17:26:35 +0000

    print 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *more_numbers - print 0 to 14
+ *@i: digit
+ *Return: result
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 14; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
