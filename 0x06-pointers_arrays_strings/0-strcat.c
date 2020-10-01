50f0f572a0e8d1890ce056e32dca32fcf1359667
Lilieyen <lnyasita@gmail.com>
2020-10-01 18:48:05 +0000

    concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,14 +7,19 @@
  *@src: source
  *Return: 0
  */
+
 char *_strcat(char *dest, char *src)
 {
 	int len;
 	int last;
 
 	len = 0;
-	while(dest[len] != '\0')
+	while (dest[len] != '\0')
 	{
 		len++;
+	}
+	for (last = 0; src[last] != '\0'; last++, len++)
+	{
+		dest[len] = src[last];
 	}
 }
