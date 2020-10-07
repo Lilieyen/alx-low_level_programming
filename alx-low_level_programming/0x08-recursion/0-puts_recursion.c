cfdccd8ca0a15b66805d3da52c4d28b6a9d9a6c2
lnyasita@gmail.com <Lilieyen>
2020-10-07 16:35:29 +0000

    put a recursion

---
+++ alx-low_level_programming/0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *_puts_recursion - puts a recursion
+ *@s: char
+ *Return: value
+ */
+
+void _puts_recursion(char *s)
+{
+  if (s <= 1)
+    {
+      return 1;
+    }
+  return s * factorial (s - 1);
+}
