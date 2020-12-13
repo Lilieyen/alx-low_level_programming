7f3ab11475ad9e85f1536c461f1129afaa0499aa
Lilieyen <lnyasita@gmail.com>
2020-12-13 21:28:46 +0300

    return number of elements

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ *dlistint_len - print length of dlistint_t
+ *@h: head
+ *Return: number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+	const dlistint_t *temp = h;
+
+	while (temp != NULL)
+	{
+		i++;
+		temp = temp->next;
+	}
+	printf("%ld", i);
+	return (i);
+}
