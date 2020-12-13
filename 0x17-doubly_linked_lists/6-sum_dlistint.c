817791cf2e8997029d28d1c1a01bb01a45e5e2e2
Lilieyen <lnyasita@gmail.com>
2020-12-13 23:57:44 +0300

    print sum of all integers

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+  *sum_dlistint - the sum of all integers
+  *@head: the head of list.
+  *Return: the sum.
+  */
+
+int sum_dlistint(dlistint_t *head)
+{
+int sum = 0;
+dlistint_t *temp = head;
+if (!head)
+return (0);
+while (temp)
+{
+sum += temp->n;
+temp = temp->next;
+}
+return (sum);
+}
